d21b8cc0b9021db65eda0d3aa7ed621a980f6d40
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-15 13:09:03 +0600

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { get, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import "./App.css";
 
